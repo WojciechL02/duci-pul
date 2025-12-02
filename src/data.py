6f2a54a9796fd69c2ae2e56ffac5ec9a7357dc63
WojciechL02 <wojciech.lapacz02@gmail.com>
2025-12-02 17:19:52 +0100

    prepare_data refactored

---
+++ src/data.py
@@ -0,0 +1,183 @@
+from pathlib import Path
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LinearRegression
+
+
+def load_npz(path):
+    data = np.load(
+        path,
+        allow_pickle=True,
+    )
+    if np.any(np.isnan(data["data"])):
+        raise ValueError(f"NaNs in data {path}")
+    return data["data"]
+
+
+def load_files(prefix: str, data_type: str):
+    folder = Path("../data")
+    mem_pattern = f"{prefix}_*{data_type}*_mem*.npz"
+    nonmem_pattern = f"{prefix}_*{data_type}*_nonmem*.npz"
+    if data_type == "from":
+        mem_pattern = f"{prefix}_*{data_type}-mem*.npz"
+        nonmem_pattern = f"{prefix}_*{data_type}-nonmem*.npz"
+    mem_matches = list(folder.glob(mem_pattern))
+    nonmem_matches = list(folder.glob(nonmem_pattern))
+    if len(mem_matches) > 1 or len(nonmem_matches) > 1:
+        raise ValueError(f"More than one file for {prefix}-{data_type} found!")
+    return mem_matches[0], nonmem_matches[0]
+
+
+def load_data(name: str, data_type: str):
+    mem_match, nonmem_match = load_files(name, data_type)
+    members = load_npz(mem_match)
+    nonmembers = load_npz(nonmem_match)
+    return members, nonmembers
+
+
+def permute_datasets(permutation, datasets):
+    permuted = []
+    for dataset in datasets:
+        permuted.append(dataset[permutation[: len(dataset)]])
+    return permuted
+
+
+def prepare_data(name, seed, p, ss_len=2000, run_type="real"):
+    """
+    Prepare data for the experiment.
+
+    Parameters
+    ----------
+    name : str
+        The name of the dataset.
+    seed : int
+        The random seed.
+    p : float
+        Target member prevalence in the *test unlabeled* set (0..1)
+    ss_len : int
+        Size of the suspect set.
+    run_type : str
+        Type of the experiment: "real", "ae_synth", "correction".
+
+    Returns
+    -------
+    X_test  : (n_test, d) float32
+    y_test  : (n_test,)  int   # true labels: members=0, nonmembers=1
+    s_test  : (n_test,)  int   # observed NU labels for test (here: all 0 = unlabeled)
+    X_ctrl_test : (n_test, d) float32  # for correction only
+    """
+    np.random.seed(seed)
+
+    if run_type == "real":
+        X_mem, X_nonmem = load_data(name, "real")
+        X_mem_generated = X_nonmem.copy()
+        X_nonmem_generated = X_nonmem.copy()
+
+    elif run_type == "synth":
+        X_mem, X_nonmem = load_data(name, "real")
+        X_mem_generated, X_nonmem_generated = load_data(name, "from")
+    elif run_type in ("ae_synth", "correction"):
+        X_mem, X_nonmem = load_data(name, "ae")
+        X_mem_generated, X_nonmem_generated = load_data(name, "from")
+    else:
+        raise FileNotFoundError(f"No matching files found for run_type {run_type}.")
+
+    perm = np.random.permutation(
+        max(len(X_mem), len(X_nonmem), len(X_mem_generated), len(X_nonmem_generated))
+    )
+    X_mem, X_nonmem, X_mem_generated, X_nonmem_generated = permute_datasets(
+        perm, [X_mem, X_nonmem, X_mem_generated, X_nonmem_generated]
+    )
+
+    n_pos_test = int(ss_len * p)  # members inside suspect set
+    n_unl_test_nonmem = ss_len - n_pos_test  # non-members inside test unlabeled
+
+    if run_type == "real":
+        X_test_nonmem = np.concatenate(
+            [
+                X_mem_generated[: int(ss_len / 2)],
+                X_nonmem_generated[int(ss_len / 2) : ss_len],
+            ]
+        )
+    else:
+        X_test_nonmem = np.concatenate(
+            [
+                X_mem_generated[:n_pos_test],
+                X_nonmem_generated[ss_len : ss_len + n_unl_test_nonmem],
+            ]
+        )
+
+    X_test = np.concatenate(
+        [
+            X_test_nonmem,
+            X_mem[:n_pos_test],
+            X_nonmem[ss_len : ss_len + n_unl_test_nonmem],
+        ]
+    )
+
+    y_test = np.concatenate(
+        [
+            np.ones(ss_len, dtype=int),  # N-labeled
+            np.zeros(n_pos_test, dtype=int),  # U-members
+            np.ones(n_unl_test_nonmem, dtype=int),  # U-non-members
+        ]
+    )
+
+    s_test = np.concatenate(
+        [
+            np.ones(ss_len, dtype=int),  # N-labeled
+            np.zeros(n_pos_test, dtype=int),  # U-members
+            np.zeros(n_unl_test_nonmem, dtype=int),  # U-non-members
+        ]
+    )
+
+    X_test = X_test.squeeze(1)
+    scaler = MinMaxScaler()
+    X_test = scaler.fit_transform(X_test)
+
+    if run_type == "correction":
+        control_name = (
+            "ControlSEnsemble_coco" if "uvit" in name else "ControlSEnsemble_in"
+        )
+        mem, nonmem = load_files(control_name, "ae")
+        synth_mem, synth_nonmem = load_files(control_name, "from")
+        X_ctrl_ae_mem = load_npz(mem)
+        X_ctrl_ae_nonmem = load_npz(nonmem)
+        X_ctrl_mem_generated = load_npz(synth_mem)
+        X_ctrl_nonmem_generated = load_npz(synth_nonmem)
+        (
+            X_ctrl_ae_mem,
+            X_ctrl_ae_nonmem,
+            X_ctrl_mem_generated,
+            X_ctrl_nonmem_generated,
+        ) = permute_datasets(
+            perm,
+            [
+                X_ctrl_ae_mem,
+                X_ctrl_ae_nonmem,
+                X_ctrl_mem_generated,
+                X_ctrl_nonmem_generated,
+            ],
+        )
+        X_ctrl_test_nonmem = np.concatenate(
+            [
+                X_ctrl_mem_generated[:n_pos_test],
+                X_ctrl_nonmem_generated[ss_len : ss_len + n_unl_test_nonmem],
+            ]
+        )
+
+        X_ctrl_test = np.concatenate(
+            [
+                X_ctrl_test_nonmem,
+                X_ctrl_ae_mem[:n_pos_test],
+                X_ctrl_ae_nonmem[ss_len : ss_len + n_unl_test_nonmem],
+            ]
+        )
+        X_ctrl_test = X_ctrl_test.squeeze(1)
+        ols = LinearRegression().fit(X_ctrl_test, X_test)
+        Xhat_test = ols.predict(X_ctrl_test)  # take only ctrl features explaining MIA
+        scaler = MinMaxScaler()
+        X_ctrl_test = scaler.fit_transform(Xhat_test)
+        return X_test, y_test, s_test, X_ctrl_test
+
+    return X_test, y_test, s_test, None
